348e1521e23a5f3d377efa5876e449f8f5b4b3f7
Ryan Lau <rlau1185@jh-mba158-14.local>
2018-10-09 14:53:01 -0500

    Added UI actions and Outlets

--- Commisson/ViewController.swift
+++ Commisson/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var totalPayLabel: UILabel!
+    @IBOutlet weak var commissionTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
-
+    @IBAction func calculateOnTapped(_ sender: Any) {
+    }
+    
 }
 
