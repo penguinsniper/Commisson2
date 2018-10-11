c02434e883fc39b2e84ba35a287ca6bc812f9ea3
Ryan Lau <rlau1185@jh-mba158-14.d214.org>
2018-10-11 14:30:17 -0500

    added format stuff

--- Commisson/ViewController.swift
+++ Commisson/ViewController.swift
@@ -19,11 +19,22 @@ class ViewController: UIViewController {
     }
 
     @IBAction func calculateOnTapped(_ sender: Any)
-     {
-        
+   {
+    let comPay = getInput()
+    let totalPay = comPay + Double(basepay)
+    let fromattedPay = String(format: "%.2f" , totalPay)
+    totalPayLabel.text = "Total Pay = $\(totalPay)"
     }
-    func getInput(){
+    func getInput() -> Double
+    {
+        if let data = commissionTextField.text, let commissonPay = Double(data){
+            return commissonPay
+    } else {
+            return 0
     
-   }
+        }
+    }
 
-}
+ }
+
+
